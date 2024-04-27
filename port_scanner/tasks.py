afeb1e209aa840cd1f259cb445c1b65b77597a3f
ZacharyZcR <2903735704@qq.com>
2024-04-27 09:57:14 +0800

    refactor: multi Port Scan

--- port_scanner/tasks.py
+++ port_scanner/tasks.py
@@ -10,58 +10,58 @@ import os
 
 @shared_task(bind=True)
 def scan_ports(self, target, ports):
-    # 首先创建一个新的ScanJob实例，初始化状态为'R'（Running），并立即保存
+    # 创建一个新的ScanJob实例
     scan_job = ScanJob.objects.create(target=target, status='R', task_id=self.request.id)
-
-    # 创建临时文件名
     temp_file_path = f"/tmp/{scan_job.task_id}.txt"
+    all_ports_found = set()  # 用来存储所有扫描到的端口和服务
 
     try:
-        # 构建nmap命令，并将输出重定向到临时文件
-        cmd = f"nmap -n --resolve-all -Pn --min-hostgroup 64 --max-retries 0 --host-timeout 10m --script-timeout 3m --version-intensity 9 --min-rate 10000 -T4 {target} -p {ports} -oN {temp_file_path}"
-        # 执行命令
-        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        _, stderr = process.communicate()
+        for _ in range(5):  # 执行五次扫描
+            # 构建并执行nmap命令
+            cmd = f"nmap -n --resolve-all -Pn --min-hostgroup 64 --max-retries 0 --host-timeout 10m --script-timeout 3m --version-intensity 9 --min-rate 10000 -T4 {target} -p {ports} -oN {temp_file_path}"
+            process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            _, stderr = process.communicate()
 
-        if stderr or process.returncode != 0:
-            scan_job.status = 'E'  # 更新状态为错误
-            scan_job.error_message = f'Nmap 扫描失败: {stderr.decode()}'
-        else:
-            # 从临时文件中读取输出
+            if stderr or process.returncode != 0:
+                scan_job.status = 'E'
+                scan_job.error_message = f'Nmap 扫描失败: {stderr.decode()}'
+                break
+
+            # 读取输出并查找开放端口
             with open(temp_file_path, 'r') as file:
                 output = file.read()
-
-            # 使用正则表达式查找开放的端口
             open_ports = re.findall(r'(\d+)/tcp\s+open\s+(\S+)', output)
-            if not open_ports:
-                scan_job.status = 'E'
-                scan_job.error_message = f'没有找到开放的端口 {target}。'
-            else:
-                for port, service in open_ports:
-                    # 创建Port对象
+            new_ports_found = set(open_ports) - all_ports_found
+
+            if new_ports_found:
+                all_ports_found.update(new_ports_found)  # 更新所有找到的端口集合
+                for port, service in new_ports_found:
                     new_port = Port.objects.create(
                         scan_job=scan_job,
                         port_number=int(port),
                         service_name=service,
                         protocol='tcp',
                         state='open',
-                        ip_address=target  # 添加IP地址
+                        ip_address=target
                     )
-                    # 测试HTTP
+                    # 测试协议
                     new_port.http_code, new_port.http_title = check_protocol(target, port, 'http')
-                    # 测试HTTPS
                     new_port.https_code, new_port.https_title = check_protocol(target, port, 'https')
-
                     new_port.save()
 
-                scan_job.status = 'C'  # 更新状态为完成
+        if not all_ports_found:
+            scan_job.status = 'E'
+            scan_job.error_message = '没有找到开放的端口。'
+        else:
+            scan_job.status = 'C'
+
     except Exception as e:
-        scan_job.status = 'E'  # 更新状态为错误
+        scan_job.status = 'E'
         scan_job.error_message = f'扫描过程中发生异常: {str(e)}'
+
     finally:
-        scan_job.end_time = timezone.now()  # 记录结束时间
-        scan_job.save()  # 明确保存ScanJob实例的更改
-        # 清理临时文件
+        scan_job.end_time = timezone.now()
+        scan_job.save()
         os.remove(temp_file_path)
 
         if scan_job.status == 'E':
