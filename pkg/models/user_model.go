31e165cd7468b6ce7a9868a6560e302fb59edfbd
ZacharyZcR <2903735704@qq.com>
2024-10-29 20:09:25 +0800

    perf: 重命名模块

--- pkg/models/user_model.go
+++ pkg/models/user_model.go
@@ -1,4 +1,13 @@
+// CyberEdge/pkg/models/auth.go
+
 package models
+
+// User 代表用户的结构体
+type User struct {
+	Account    string `bson:"account" json:"account"`
+	Secret     string `bson:"secret" json:"-"`
+	LoginCount int    `bson:"loginCount" json:"loginCount"`
+}
 
 // TOTPValidationRequest 定义了TOTP验证请求的结构
 type TOTPValidationRequest struct {
