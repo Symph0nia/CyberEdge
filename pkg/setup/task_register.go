6f02cb0a5afe2d0132cb6afe9e02457f14acf7a5
ZacharyZcR <2903735704@qq.com>
2024-12-18 09:32:12 +0800

    refactor: 去掉测试任务

--- pkg/setup/task_register.go
+++ pkg/setup/task_register.go
@@ -9,15 +9,10 @@ import (
 func InitTaskHandler(taskDAO *dao.TaskDAO, targetDAO *dao.TargetDAO, resultDAO *dao.ResultDAO) *tasks.TaskHandler {
 	taskHandler := tasks.NewTaskHandler()
 
-	// 注册 Ping 任务处理函数
-	pingTask := tasks.NewPingTask(taskDAO)
-	httpxTask := tasks.NewHttpxTask(taskDAO)
-	subfinderTask := tasks.NewSubfinderTask(taskDAO, targetDAO, resultDAO) // 传入 resultDAO
+	subfinderTask := tasks.NewSubfinderTask(taskDAO, targetDAO, resultDAO)
 	nmapTask := tasks.NewNmapTask(taskDAO, targetDAO, resultDAO)
 	FfufTask := tasks.NewFfufTask(taskDAO, targetDAO, resultDAO)
 
-	taskHandler.RegisterHandler(tasks.TaskTypePing, pingTask.Handle)
-	taskHandler.RegisterHandler(tasks.TaskTypeHttpx, httpxTask.Handle)
 	taskHandler.RegisterHandler(tasks.TaskTypeSubfinder, subfinderTask.Handle)
 	taskHandler.RegisterHandler(tasks.TaskTypeNmap, nmapTask.Handle)
 	taskHandler.RegisterHandler(tasks.TaskTypeFfuf, FfufTask.Handle)
